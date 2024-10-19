d8df52e305fb2b76519db03c3990e986f270d76d
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-10-19 17:04:15 +0200

    minor text change
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -84,4 +84,4 @@ done
 
 source "$BOOTSTRAPPER_DIR/_motd.sh"
 
-echo -e "${CYAN}${BOLD}[Sc-Toolbox]${RESET} Bootstrapper: ${SUCCESS_EMOJI} Sourced all [${YELLOW}${iterator}${RESET}] .sh files in ${BOOTSTRAPPER_DIR}"
+echo -e "${CYAN}${BOLD}[Sc-Toolbox]${RESET} Bootstrapper: ${SUCCESS_EMOJI} Sourced all [${YELLOW}${iterator}${RESET}] .sh files in ${BOOTSTRAPPER_DIR}/*.sh"
